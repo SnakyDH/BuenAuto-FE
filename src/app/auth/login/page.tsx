70aa3ce8ec63bdf8f192ccdd7e78d1744a617a3a
Juan Pablo Bautista <juan pablo bautista>
2023-11-28 11:13:58 -0500

    Login implementado en rama nueva

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,57 +1,40 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import './stylesLogin.css';
 
 export default function LoginPage() {
   return (
-    <main className='flex justify-center items-center h-screen bg-blue-300'>
-      <aside className='bg-indigo-700 self-center h-1/2 rounded-s-2xl'>
-        <Image
-          className='rounded-s-2xl'
-          src='/login-car.png'
-          width={350}
-          height={500}
-          style={{ objectFit: 'contain' }}
-          sizes='(max-width: 768px) 100vw, 33vw'
-          alt='Car image'
-        />
-      </aside>
-      <section className='bg-white flex flex-col align-center p-10 h-1/2 rounded-e-2xl'>
-        <h2 className='text-center font-extrabold text-2xl'>Inicia Sesión</h2>
-        <form
-          className='flex flex-col align-center content-center justify-center gap-3'
-          action=''
-        >
-          <div className='flex flex-col'>
-            <label className='font-semibold text-lg' htmlFor='email'>
-              Correo:
-            </label>
-            <input
-              className='rounded-xl p-3'
-              type='email'
-              name='email'
-              id='email'
-              placeholder='example@example.com'
-            />
-          </div>
-          <div className='flex flex-col'>
-            <label className='font-semibold text-lg' htmlFor='password'>
-              Contraseña:
-            </label>
-            <input
-              className='rounded-xl p-3'
-              type='password'
-              name='password'
-              id='password'
-              placeholder='********'
-            />
-          </div>
-          <button
-            className='bg-indigo-900 rounded-xl p-3 font-semibold text-lg text-white'
-            type='submit'
-          >
-            Iniciar Sesión
-          </button>
-        </form>
-      </section>
-    </main>
+    <div className="contenedor">
+    <div className="caja-formulario">
+
+
+        <div className="contenedor-login" id="login">
+            <div className="encabezado">
+                <span>No tienes una cuenta <Link href='/registro'> Regístrate </Link> </span>
+                <h1 className="titulo">Inicio de Sesión</h1>
+            </div>
+            <div className="caja-entrada">
+                <input type="text" className="entrada" placeholder="Nickname o Email" />
+                <i className="bx bx-user"></i>
+            </div>
+            <div className="caja-entrada">
+                <input type="password" className="entrada" placeholder="Contraseña"/>
+                <i className="bx bx-lock-alt"></i>
+            </div>
+            <div className="caja-entrada">
+                <input type="submit" className="boton-enviar" value="Iniciar Sesión"/>
+            </div>
+            <div className="dos-columnas">
+                <div className="uno">
+                    <input type="checkbox" id="login-check"/>
+                    <label htmlFor="login-check"> Recuérdame</label>
+                </div>
+                <div className="dos">
+                    <label><a href="#">¿Olvidaste tu Contraseña?</a></label>
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
   );
 }
