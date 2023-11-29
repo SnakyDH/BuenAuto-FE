61801e985932629aac1f5c470964afacbb103da1
Juan Pablo Bautista <juan pablo bautista>
2023-11-28 19:34:49 -0500

    cambios realizados al home de la aplicacion y desistalacion de logos que no se procesas con react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import './globals.css';
-import 'boxicons/css/boxicons.min.css';
 
 export default function Home() {
   return (
-    <div id='__next'>
+    <>
       <header>
         <div id='barra-menu' className='barra bar-menu menu-icon'>
           &#9776
@@ -20,18 +19,12 @@ export default function Home() {
             <i className='bx bx-menu'></i>
           </div>
 
-          <ul className='navbar'>
+          <ul className='navbar hidden sm:visible'>
             <li>
               <a href='#home'> Home </a>
             </li>
             <li>
               <a href='#service'> Servicios </a>
-            </li>
-            <li>
-              <a href='#vehiculos'> Vehiculos </a>
-            </li>
-            <li>
-              <a href='#reseñas'> Venta </a>
             </li>
             <li>
               <a href='#acerca'> Acerca de nosotros </a>
@@ -60,9 +53,6 @@ export default function Home() {
             la mejor Concesionaria de la ciudad Lorem ipsum dolor sit amet
             consectetur adipisicing elit. Quisquam, voluptatum.
           </p>
-          <a href='#' className='btn btn-home'>
-            Ver Vehiculos
-          </a>
         </div>
       </section>
 
@@ -104,188 +94,9 @@ export default function Home() {
               sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
             </p>
           </div>
-
-          <div className='caja-servicio'>
-            <i className='bx bxs-car-crash'></i>
-            <h3> Servicio Postventa </h3>
-            <p>
-              {' '}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
-            </p>
-          </div>
         </div>
       </section>
 
-      {/* Seccion Vehiculos */}
-      <section className='vehiculos' id='vehiculos'>
-        <div className='tituloServicios'>
-          <span> Vehiculos disponibles </span>
-          <h2>
-            Explora en toda nuestra variedad de Vehiculos nuevos o usados segun
-            tu conveniencia asi como presupuesto{' '}
-          </h2>
-        </div>
-
-        <div className='contenedor-vehiculos'>
-          <div className='caja-vehiculos'>
-            <div className='img-vehiculos'>
-              <Image src='/img/car1.jpg' width={640} height={427} alt='' />
-            </div>
-            <p>2019</p>
-            <h3>Toyota Corolla</h3>
-            <h2>
-              $110.000.000 | $ 1.500.000 <span> /Cuotas mensuales</span>
-            </h2>
-            <a href='#' className='btn'>
-              Compralo ahora
-            </a>
-          </div>
-
-          <div className='caja-vehiculos'>
-            <div className='img-vehiculos'>
-              <Image src='/img/car2.jpg' width={640} height={427} alt='' />
-            </div>
-            <p>2019</p>
-            <h3>Toyota Corolla</h3>
-            <h2>
-              $110.000.000 | $ 1.500.000 <span> /Cuotas mensuales</span>
-            </h2>
-            <a href='#' className='btn'>
-              Compralo ahora
-            </a>
-          </div>
-
-          <div className='caja-vehiculos'>
-            <div className='img-vehiculos'>
-              <Image src='/img/car3.jpg' width={640} height={427} alt='' />
-            </div>
-            <p>2019</p>
-            <h3>Toyota Corolla</h3>
-            <h2>
-              $110.000.000 | $ 1.500.000 <span> /Cuotas mensuales</span>
-            </h2>
-            <a href='#' className='btn'>
-              Compralo ahora
-            </a>
-          </div>
-
-          <div className='caja-vehiculos'>
-            <div className='img-vehiculos'>
-              <Image src='/img/car4.jpg' width={640} height={427} alt='' />
-            </div>
-            <p>2019</p>
-            <h3>Toyota Corolla</h3>
-            <h2>
-              $110.000.000 | $ 1.500.000 <span> /Cuotas mensuales</span>
-            </h2>
-            <a href='#' className='btn'>
-              Compralo ahora
-            </a>
-          </div>
-
-          <div className='caja-vehiculos'>
-            <div className='img-vehiculos'>
-              <Image src='/img/car5.jpg' width={640} height={427} alt='' />
-            </div>
-            <p>2019</p>
-            <h3>Toyota Corolla</h3>
-            <h2>
-              $110.000.000 | $ 1.500.000 <span> /Cuotas mensuales</span>
-            </h2>
-            <a href='#' className='btn'>
-              Compralo ahora
-            </a>
-          </div>
-
-          <div className='caja-vehiculos'>
-            <div className='img-vehiculos'>
-              <Image src='/img/car6.jpg' width={640} height={427} alt='' />
-            </div>
-            <p>2019</p>
-            <h3>Toyota Corolla</h3>
-            <h2>
-              $110.000.000 | $ 1.500.000 <span> /Cuotas mensuales</span>
-            </h2>
-            <a href='#' className='btn'>
-              Compralo ahora
-            </a>
-          </div>
-        </div>
-      </section>
-
-      {/* Seccion Reseñas */}
-
-      <section className='Reseñas' id='reseñas'>
-        <div className='tituloServicios'>
-          <span> Reseñas </span>
-          <h2>Experiencias de nuestros usuarios</h2>
-        </div>
-
-        <div className='contenedor-reseñas'>
-          <div className='caja-reseñas'>
-            <div className='img-reseñas'>
-              <Image src='/img/rev1.jpg' width={640} height={959} alt='' />
-            </div>
-            <div className='estrellas'>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bx-star'></i>
-            </div>
-            <h3>Carlos Grillo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Quisquam, voluptatum.
-            </p>
-          </div>
-
-          <div className='caja-reseñas'>
-            <div className='img-reseñas'>
-              <Image src='/img/rev2.jpg' width={640} height={959} alt='' />
-            </div>
-            <div className='estrellas'>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-            </div>
-            <h3>Pepe Perez</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Quisquam, voluptatum.
-            </p>
-          </div>
-
-          <div className='caja-reseñas'>
-            <div className='img-reseñas'>
-              <Image src='/img/rev3.jpg' width={640} height={959} alt='' />
-            </div>
-            <div className='estrellas'>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star'></i>
-              <i className='bx bxs-star-half'></i>
-            </div>
-            <h3>Angelica Martinez</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Quisquam, voluptatum.
-            </p>
-          </div>
-        </div>
-      </section>
       {/* Seccion Acerca */}
       <section className='AcercaDe' id='acerca'>
         <div className='tituloServicios'>
@@ -311,11 +122,11 @@ export default function Home() {
               sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
             </p>
             <a href='#' className='btn'>
-              Leer mas
+              Regresar
             </a>
           </div>
         </div>
       </section>
-    </div>
+    </>
   );
 }
